c53718b98954ee93692ce6c0c81a62b7c942461c
Felix <felixschlitter@gmail.com>
2014-04-16 08:42:55 +1200

    Fix build script and bump version

--- build/_distribute.bat
+++ build/_distribute.bat
@@ -1,31 +1,46 @@
 @ECHO OFF
+
 REM GO TO ROOT
+REM ---------------------------------------------------------------------------- 
 CD /D ".."
+FOR /F %%i IN ('CHDIR') DO SET CWD=%%i
 
 REM SET GLOBALS
+REM ---------------------------------------------------------------------------- 
 SET "DEST=QSM-REV-%1"
 SET "XMNGR=%2"
 SET "ZIP7=%3"
-SET FLAGS=/Y
+SET "MXI=%CWD%\QuickSaveMaps.mxi"
+SET "MXP=%CWD%\QuickSaveMaps.mxp"
 
 REM CREATE MXP
-%XMNGR% -suppress -package mxi=QuickSaveMaps.mxi mxp=QuickSaveMaps.mxp
+REM ---------------------------------------------------------------------------- 
+echo %MXI%
+
+REM For some reasons -suppress causes the command not to work...
+REM %XMNGR% -suppress -package mxi=%MXI% mxp=%MXP%
+%XMNGR% -package mxi=%MXI% mxp=%MXP%
 
 REM DELETE DIR
+REM ---------------------------------------------------------------------------- 
 rmdir %DEST% /S /Q
 mkdir %DEST%
 
 REM COPY FILES
-xcopy "README.txt" %DEST% %FLAGS%
-xcopy "QuickSaveMaps.atn" %DEST% %FLAGS%
-xcopy "quickSaveMaps.mxp" %DEST% %FLAGS%
+REM ---------------------------------------------------------------------------- 
+xcopy "README.md"         %DEST% /Y
+xcopy "QuickSaveMaps.atn" %DEST% /Y
+xcopy "QuickSaveMaps.mxp" %DEST% /Y
 
 REM REMOVE MXP FILE
-rm QuickSaveMaps.mxp
+REM ---------------------------------------------------------------------------- 
+RM QuickSaveMaps.mxp
 
 REM ZIP THE FOLDER UP
-rm %DEST%.zip
+REM ---------------------------------------------------------------------------- 
+RM %DEST%.zip
 %ZIP7% a -Y %DEST%.zip %DEST%
 
 REM REMOVE DEST DIR
-rmdir %DEST% /S /Q
+REM ---------------------------------------------------------------------------- 
+RMDIR %DEST% /S /Q
